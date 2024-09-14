99327fd4700353dd5265ed322fe1a254e7c2d034
MasterZendika <125993067+MasterZendika@users.noreply.github.com>
2024-09-14 17:27:07 -0600

    Cambios

--- src/server/nessieManager.ts
+++ src/server/nessieManager.ts
@@ -2,15 +2,23 @@ import { env } from "~/env";
 
 export const nessieManager = {
     async getCustomerById(id: string) {
-
         const response = await fetch(`http://api.nessieisreal.com/accounts/${id}/customer?key=${env.NESSIE_API_KEY}`);
 
         if (!response.ok) {
             throw new Error("Failed to fetch customers");
         }
 
-        console.log("response", response);
+        return response.json();
+    },
+
+    async getTransactionsByAccountId(accountId: string) {
+        const response = await fetch(`http://api.nessieisreal.com/accounts/${accountId}/transactions?key=${env.NESSIE_API_KEY}`);
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch transactions");
+        }
 
         return response.json();
     }
 }
+ 
