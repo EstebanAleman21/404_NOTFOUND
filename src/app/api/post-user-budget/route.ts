65f20f5d5dd2328e22cd6a2b74ec0666f35b42de
Sebastian Rosas Maciel <rosassebastian2003@gmail.com>
2024-09-15 06:09:58 -0600

    No es posible introducir categorias repetidas en el presupuesto

--- src/app/api/post-user-budget/route.ts
+++ src/app/api/post-user-budget/route.ts
@@ -10,7 +10,6 @@ interface RequestBody {
   isValid: boolean;
 }
 
-
 export async function POST(request: Request) {
   const session = await getServerAuthSession();
   const prisma = new PrismaClient();
@@ -21,9 +20,21 @@ export async function POST(request: Request) {
 
   try {
     const body = await request.json() as unknown;
-
     const { amount, period, category, isValid } = body as RequestBody;
 
+    // Verificar si la categoría ya existe para el usuario
+    const existingCategory = await prisma.budget.findFirst({
+      where: {
+        userId: session.user.id,
+        category: category,
+      },
+    });
+
+    if (existingCategory) {
+      return NextResponse.json({ success: false, message: 'Category already exists' }, { status: 400 });
+    }
+
+    // Crear la nueva categoría si no existe
     await prisma.budget.create({
       data: {
         userId: session.user.id,
@@ -33,8 +44,10 @@ export async function POST(request: Request) {
         isValid,
       },
     });
+
     return NextResponse.json({ success: true });
   } catch (error: unknown) {
-    return NextResponse.json({ success: false });
+    console.error('Error creating budget:', error);
+    return NextResponse.json({ success: false, message: 'Failed to create budget' }, { status: 500 });
   } 
 }
